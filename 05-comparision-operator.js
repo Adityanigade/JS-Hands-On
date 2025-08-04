394675574124039d3b1f0c60f4142f91c47df7af
Adityanigade <adityanigade142@gmail.com>
2025-08-04 21:44:09 +0530

    Create 05-comparision-operator.js

---
+++ 05-comparision-operator.js
@@ -0,0 +1,99 @@
+console.log("===== == Equal or Double Equal or loosely equal Operator ========");
+var numOne = 10;
+var numTwo = 10
+var numThree = "10";
+var numFour = 20;
+console.log(` Is numOne equal to numTwo ==> ${numOne==numTwo}`);
+console.log(` Is numOne equal to numThree ==> ${numOne==numThree}`);
+
+console.log(` Is numOne equal to numFour ==> ${numOne==numFour}`);
+
+
+
+console.log("================(==equal / double equal / loosly equal operator)==========");
+//check only value not check data type
+
+var num1 = 10;                    // number
+var num2 = 10;                    // number
+var result = num1 == num2;
+console.log(`Result is: ${result}`);               // true
+
+console.log("================(==equal operator with same data type)==========");
+
+var num1 = 10;                    // number
+var num2 = 10;                    // number
+var result = num1 == num2;
+console.log(`Result is: ${result}`);                   // true
+
+
+console.log("================(==equal operator with same data type)==========");
+
+var num1 = 20;                    // number
+var num2 = 10;                    // number
+var result = num1 == num2;
+console.log(`Result is: ${result}`);                   // true
+
+
+console.log("================(==equal operator with diff data type)==========");
+
+var num1 = 10;                      // number                 
+var num2 = "10";                   //string
+var result = num1 == num2;
+console.log(`Result is: ${result}`);                    // true
+
+
+
+console.log("===== === Triple Equal or Strictly Equal or Strongly equal Operator ========");
+
+console.log(` Is numOne equal to numTwo ==> ${numOne===numTwo}`);
+console.log(` Is numOne equal to numThree ==> ${numOne===numThree}`);
+
+console.log(` Is numOne equal to numFour ==> ${numOne===numFour}`);
+
+
+
+console.log("================(=== triple equal / strictly equal operator)==========");
+// check value and check data type
+
+
+
+console.log("===== > Greater than Operator ========");
+
+var numOne = 10;
+var numTwo = 11;
+var numThree = 11;
+console.log(` Is numOne greater than numTwo ==> ${numOne > numTwo}`);
+
+console.log(` Is numTwo greater than numThree ==> ${numTwo > numThree}`);
+
+
+console.log("===== >= Greater than Equal Operator ========");
+
+var numOne = 10;
+var numTwo = 11;
+var numThree = 11;
+console.log(` Is numOne greater than Equal numTwo ==> ${numOne >= numTwo}`);
+
+console.log(` Is numTwo greater than Equal numThree ==> ${numTwo >= numThree}`);
+
+
+
+
+console.log("===== < Less than Operator ========");
+var numOne = 10;
+var numTwo = 11;
+var numThree = 11;
+console.log(` Is numOne Less than numTwo ==> ${numOne < numTwo}`);
+
+console.log(` Is numTwo Less than numThree ==> ${numTwo < numThree}`);
+
+console.log("===== <= Less than Equal Operator ========");
+var numOne = 10;
+var numTwo = 11;
+var numThree = 11;
+console.log(` Is numOne Less than Equal numTwo ==> ${numOne <= numTwo}`);
+
+console.log(` Is numTwo Less than Equal numThree ==> ${numTwo <= numThree}`);
+
+
+
