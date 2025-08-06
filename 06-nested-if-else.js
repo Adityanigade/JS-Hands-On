1e20b952dabfe36505ad123ed0932698ea04dd82
Adityanigade <adityanigade142@gmail.com>
2025-08-06 21:24:33 +0530

    statements
    statements

---
+++ 06-nested-if-else.js
@@ -0,0 +1,20 @@
+console.log("======= START ==========");
+
+var isPlaced = true;
+if (isPlaced) {
+    console.log("With first salary");
+    console.log("I will buy some cloths for my family");
+
+    var isProposedAccepte=true;
+    if (isProposedAccepte) {
+        console.log("I will also buy some cloths and gifts for her");
+        
+    } else {
+       console.log("I willl find other");
+        
+    }
+} else {
+    console.log("I will return to my home town....");
+    console.log("I will start Farming");
+}
+console.log("======= END ==========");
